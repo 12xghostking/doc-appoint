e75bf7964865171832f53442314be25462928a1c
anmoln <sirkinggreat@gmail.com>
2023-09-14 23:16:18 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage.js';
+import SignUp from './components/SignUp/SignUp.js';
+import Login from './components/Login/Login.js';
+import DoctorDashboard from './components/DocDash/DocDash.js';
+import PatientDashboard from './components/PaDash/PaDash.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/Doctor" element={<DoctorDashboard />} />
+      <Route path="/Patient" element={<PatientDashboard />} />
+   </Routes>
   );
 }
 
